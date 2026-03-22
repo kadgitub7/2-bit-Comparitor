a04545145f82976a79bffbc04e88f9ba6e90369a
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-22 09:13:33 -0400

    implemented testbench

---
+++ twoBitDecoder_tb.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module twoBitDecoder_tb();
+    
+    reg[2:0] A,B;
+    wire Aless,Aequal,Agreater;
+    
+    twoBitDecoder uut(A,B,Aless,Aequal,Agreater);
+    
+    integer i;
+    
+    initial begin
+        for(i=0;i<16;i=i+1)begin
+            {A,B} = i;
+            #10 $display("A = %b, B = %b, Aless = %b, Aequal = %b, Agreater = %b", A,B,Aless,Aequal,Agreater);
+        end
+    end
+        
+endmodule
